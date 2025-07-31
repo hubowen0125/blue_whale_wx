716957ddc1949c0d57f82f58c2811c093e0e7c94
hubowen0125 <69292400+hubowen0125@users.noreply.github.com>
2025-07-31 22:36:35 +0800

    fix：删除不需要代码

--- src/packagingStation/printerSettings/util.d.ts
+++
@@ -1,6 +0,0 @@
-// src/types/util.d.ts 或 src/packagingStation/printerSettings/types.d.ts
-declare module './util.js' {
-    export function hexStringToArrayBuffer(str: string): ArrayBuffer;
-    export function hexStringToBuff(str: string): ArrayBuffer;
-    export function send0X0A(): ArrayBuffer;
-}
